73adbd8717b1cbb742d8764f3486a39d9f551c7d
richard <>
2005-01-11 18:32:16 +0000

    add pruneTuple.cxx

---
+++ src/pruneTuple.cxx
@@ -0,0 +1,48 @@
+#include "pipelineDatasets/pruneTuple.h"
+
+pruneTuple::~pruneTuple() { 
+  if (m_chain) delete m_chain    ;
+  if (m_newFile) delete m_newFile    ;
+
+};
+
+
+int pruneTuple::prune(UInt_t maxPerFile) {
+
+  // Concatenate AnalysisNtuple ntuple, applying cuts on active distance and
+  // number of tracks
+  //
+  // Example of usage: from CINT -
+  // .L PruneTuple.C
+  // PruneTuple("IndividualRuns/nt*.root")
+  //
+
+
+  // cd to the new file
+  m_newFile->cd();
+
+  // copy the tree entries that pass the cuts
+  TTree *newTree = m_chain->CopyTree(m_cut);
+
+  newFile->Write();
+
+  if (newTree->GetEntries() > maxPerFile) {
+    UInt_t numEntries = newtree->GetEntries();
+    UInt_t numTrees = Int_t(numEntries/maxPerFile);
+    if (numTrees*maxPerFile < numEntries) ++numTrees;
+    UInt_t i;
+    for (i=0; i<numTrees; i++) {
+      Int_t total = maxPerFile;
+      if (i==numTrees-1) total = numEntries-i*maxPerFile;
+      Int_t first = 0 + i*maxPerFile;
+      char filename[50];
+      sprintf(filename, "%s%d%s", "ntuple_prune", i, ".root");
+      //    printf("processing %d tree starting evt = %d tot = %d\n", i, first, total);
+      TFile *file=new TFile(filename,"new");
+      TTree *t = newTree->CopyTree("","", total, first);
+      file->Write();
+      delete file;
+    }
+  }
+  return 0;
+}
